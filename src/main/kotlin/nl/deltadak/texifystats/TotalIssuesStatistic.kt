f299d83d7af5f01c7c80115b9ad6880bacef3e93
Thomas <hollandpirates@gmail.com>
2020-01-21 18:20:29 +0100

    Add pagination

--- src/main/kotlin/nl/deltadak/texifystats/TotalIssuesStatistic.kt
+++ src/main/kotlin/nl/deltadak/texifystats/TotalIssuesStatistic.kt
@@ -18,69 +18,94 @@ enum class Action { OPEN, CLOSE }
 /** An issue was opened or closed. */
 data class OpenCloseEvent(val time: Instant, val action: Action)
 
+class TotalIssuesStatistic(private val githubToken: String) {
 
-fun createTotalIssuesPlot(data: ViewIssuesCountQuery.Data) {
-    val eventList = mutableListOf<OpenCloseEvent>()
+    private val eventList = mutableListOf<OpenCloseEvent>()
 
-    // Read open and close dates
-    data.repository?.issues?.nodes?.forEach {
-        val createdAt = Instant.parse(it?.createdAt.toString())
-        eventList.add(OpenCloseEvent(createdAt, Action.OPEN))
-        // Issues that are not closed are still open
-        if (it?.closedAt != null) {
-            val closedAt = Instant.parse(it.closedAt.toString())
-            eventList.add(OpenCloseEvent(closedAt, Action.CLOSE))
+    fun receiveData(data: ViewIssuesCountQuery.Data) {
+
+        val edges = data.repository?.issues?.edges
+
+        // Read open and close dates
+        edges?.forEach {
+            val node = it?.node
+            val createdAt = Instant.parse(node?.createdAt.toString())
+            eventList.add(OpenCloseEvent(createdAt, Action.OPEN))
+            // Issues that are not closed are still open
+            if (node?.closedAt != null) {
+                val closedAt = Instant.parse(node.closedAt.toString())
+                eventList.add(OpenCloseEvent(closedAt, Action.CLOSE))
+            }
         }
+
+        // If we have paginated to the end
+        if (edges?.isNullOrEmpty() == true) {
+            createPlot()
+        }
+
+        // Next page
+        val cursor = edges.last()?.cursor
+        runQuery(cursor)
     }
 
-    // Sort the events in order to walk through them and update the total issues counter for each event
-    eventList.sortBy { it.time }
+    private fun createPlot() {
+        // Sort the events in order to walk through them and update the total issues counter for each event
+        eventList.sortBy { it.time }
 
-    // List of number of open issues, at index i is the count right after the i'th event in eventList
-    val totalIssuesList = mutableListOf<Int>()
-    var counter = 0
-    for (event in eventList) {
-        if (event.action == Action.OPEN) {
-            counter++
-        } else {
-            counter--
+        // List of number of open issues, at index i is the count right after the i'th event in eventList
+        val totalIssuesList = mutableListOf<Int>()
+        var counter = 0
+        for (event in eventList) {
+            if (event.action == Action.OPEN) {
+                counter++
+            } else {
+                counter--
+            }
+            totalIssuesList.add(counter)
         }
-        totalIssuesList.add(counter)
+
+        val plotData = mapOf<String, Any>(
+                "date" to eventList.map { it.time.toEpochMilli() },
+                "count" to totalIssuesList
+        )
+
+        val plot = ggplot(plotData) + geom_line { x = "date"; y = "count" } + scale_x_datetime() + ggtitle("Total open issues over time")
+
+        showPlot(plot)
     }
 
-    val plotData = mapOf<String, Any>(
-            "date" to eventList.map { it.time.toEpochMilli() },
-            "count" to totalIssuesList
-    )
+    /**
+     * Run the GraphQL query, given a cursor.
+     */
+    fun runQuery(cursor: String? = null) {
+        val apolloClient = getApolloClient(githubToken)
 
-    val plot = ggplot(plotData) + geom_line { x = "date"; y = "count" } + scale_x_datetime() + ggtitle("Total open issues over time")
+        val query = ViewIssuesCountQuery("TeXiFy-IDEA", "Hannah-Sten", Input.fromNullable(cursor))
 
-    showPlot(plot)
+        apolloClient.query(query).enqueue(object : ApolloCall.Callback<ViewIssuesCountQuery.Data?>() {
+            override fun onResponse(dataResponse: Response<ViewIssuesCountQuery.Data?>) {
+                val data = dataResponse.data()
+
+                if (data == null) {
+                    println("No data received")
+                    println(dataResponse.errors())
+                } else {
+                    receiveData(data)
+                }
+            }
+
+            override fun onFailure(e: ApolloException) {
+                println(e.message)
+            }
+        })
+    }
 }
+
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         throw IllegalArgumentException("You need to provide the GitHub token")
     }
 
-    val apolloClient = getApolloClient(args[0])
-
-    val query = ViewIssuesCountQuery("TeXiFy-IDEA", "Hannah-Sten", Input.fromNullable(null))
-
-    apolloClient.query(query).enqueue(object : ApolloCall.Callback<ViewIssuesCountQuery.Data?>() {
-        override fun onResponse(dataResponse: Response<ViewIssuesCountQuery.Data?>) {
-            val data = dataResponse.data()
-
-            if (data == null) {
-                println("No data received")
-                println(dataResponse.errors())
-            } else {
-                createTotalIssuesPlot(data)
-            }
-        }
-
-        override fun onFailure(e: ApolloException) {
-            println(e.message)
-        }
-    })
+    TotalIssuesStatistic(args[0]).runQuery()
 }
