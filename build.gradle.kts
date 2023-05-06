97e47cdce155b65d81014327f7983735c898eb02
Thomas Schouten <hollandpirates@gmail.com>
2023-05-06 21:29:27 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 
-    val kotlinVersion = "1.5.20"
+    val kotlinVersion = "1.8.20"
 
     application
     kotlin("jvm") version kotlinVersion
@@ -8,16 +8,16 @@ plugins {
     idea
 
     // Plugin which checks for dependency updates with help/dependencyUpdates task.
-    id("com.github.ben-manes.versions") version "0.42.0"
+    id("com.github.ben-manes.versions") version "0.46.0"
 
     // Plugin which can update Gradle dependencies, use help/useLatestVersions
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
 
-    id("org.openjfx.javafxplugin") version "0.0.13"
+    id("org.openjfx.javafxplugin") version "0.0.14"
 
-    id("com.apollographql.apollo3") version "3.5.0"
+    id("com.apollographql.apollo3") version "3.8.1"
 
-    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
 }
 
 repositories {
@@ -30,17 +30,17 @@ dependencies {
     implementation(kotlin("stdlib"))
 
     // Apollo and dependencies
-    implementation("com.apollographql.apollo3:apollo-runtime:3.5.0")
-    implementation("com.squareup.okio:okio:3.2.0")
-    implementation("org.jetbrains:annotations:23.0.0")
-    testImplementation("org.jetbrains:annotations:23.0.0")
+    implementation("com.apollographql.apollo3:apollo-runtime:3.8.1")
+    implementation("com.squareup.okio:okio:3.3.0")
+    implementation("org.jetbrains:annotations:24.0.1")
+    testImplementation("org.jetbrains:annotations:24.0.1")
 
     // Lets-plot
-    implementation("org.openjfx:javafx-swing:20-ea+1")
-    implementation("org.openjfx:javafx:20-ea+1")
-    implementation("org.jetbrains.lets-plot:lets-plot-common:2.4.0")
-    implementation("org.jetbrains.lets-plot:lets-plot-jfx:2.4.0")
-    implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.0.0")
+    implementation("org.openjfx:javafx-swing:21-ea+5")
+    implementation("org.openjfx:javafx:21-ea+5")
+    implementation("org.jetbrains.lets-plot:lets-plot-common:3.2.0-rc1")
+    implementation("org.jetbrains.lets-plot:lets-plot-jfx:3.2.0-rc1")
+    implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.3.0")
 }
 
 apollo {
@@ -58,10 +58,6 @@ javafx {
 // Required by the GitHub Action
 application {
     mainClass.set("nl.deltadak.texifystats.LetsPlotExample")
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
 }
 
 ktlint {
